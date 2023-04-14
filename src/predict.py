95f0baa295abb384e761a8900a562256b2b4a5da
kevinmfreire <kfreirea95@gmail.com>
2023-04-14 18:04:06 -0400

    script for classifying input text

--- src/predict.py
+++ src/predict.py
@@ -10,7 +10,20 @@ from pre_process import text_preprocessor
 
 def classify(text, tf_idf, model):
     '''
-    Pipeline for classifying input text and providing sentiment.
+    The classify function takes in three arguments: 
+        * text: A string of text that the function classifies.
+        * tf_idf: A fitted TfidfVectorizer object that transforms the input text into a vector of tf-idf features.
+        * model: A trained machine learning model that the function uses to classify the input text
+
+    The function first initializes a dictionary called label_decoder, which maps integer labels to sentiment labels of "negative", "neutral", or "positive".
+    The input text is preprocessed using the text_preprocessor function, which is not shown in the code snippet. The preprocessed text is then stored in a list.
+    The tf_idf object is used to transform the preprocessed text into a vector of features.
+
+    Returns: 
+        * The sentiment label is returned as a string.
+
+    The model object is used to predict the label of the input text based on the vector of features. The predicted label is an integer value.
+    The predicted integer label is then used to look up the corresponding sentiment label in the label_decoder dictionary. 
     '''
     label_decoder = {0: "negative", 1: "neutral", 2: "positive"}
 
